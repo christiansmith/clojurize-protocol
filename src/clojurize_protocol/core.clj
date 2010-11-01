6ec071b17dfe1d6ae780ab63d8602e80482be8c5
Christian Smith <http://github.com/christiansmith>
2010-11-01 11:44:29 -0700

    Initial commit

---
+++ src/clojurize_protocol/core.clj
@@ -0,0 +1,1 @@
+(ns clojurize-protocol.core)
---
+++ test/clojurize_protocol/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojurize-protocol.test.core
+  (:use [clojurize-protocol.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
