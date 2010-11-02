a9f97403ff33b51bfea2c3b612e563afc3e3bcb1
Christian Smith <http://github.com/christiansmith>
2010-11-02 12:36:36 -0700

    renamed clojurize_protocol directories to clojurize and cleaned up namespaces

---
+++ test/clojurize/test/core.clj
@@ -0,0 +1,3 @@
+(ns clojurize.test.core
+  (:use [clojurize.core] :reload)
+  (:use [clojure.test]))
--- test/clojurize_protocol/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clojurize-protocol.test.core
-  (:use [clojurize-protocol.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
