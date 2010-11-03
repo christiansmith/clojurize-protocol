8727a9d928e466efaecdcee5a4e76049011f8be9
Christian Smith <http://github.com/christiansmith>
2010-11-03 13:48:47 -0700

    README edits

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clojurize-protocol "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "The Clojurize Protocol"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
